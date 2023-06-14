5bb261840e6ec3da4e7feef802de6ea45dbca2f5
ademirtas04 <59454552+ademirtas04@users.noreply.github.com>
2023-06-14 15:39:58 -0400

    Added Configuration for 12 Plates

--- bio_workcell/multi_growth_curve.py
+++ bio_workcell/multi_growth_curve.py
@@ -18,24 +18,51 @@ INCUBATION_TIME_SECONDS = INCUBATION_TIME_MINUTES * 60
 TARGETED_PROCESS_TIME_MINUTES = 60
 TARGETED_PROCESS_TIME_SECONDS = TARGETED_PROCESS_TIME_MINUTES * 60
 HIDEX_IDLE_THRESHOLD_SECONDS = 3600
+
+COMPLETE_HUDSON_SETUP_FILE_PATH = '/home/rpl/workspace/BIO_workcell/bio_workcell/workflows/growth_curve/complete_hudson_setup.yaml'
+STREAMLINED_HUDSON_SETUP_FILE_PATH = '/home/rpl/workspace/BIO_workcell/bio_workcell/workflows/growth_curve/streamlined_hudson_setup.yaml'
+SETUP_GROWTH_MEDIA_FILE_PATH = '/home/rpl/workspace/BIO_workcell/bio_workcell/workflows/growth_curve/setup_growth_media.yaml'
+
+CREATE_PLATE_T0_FILE_PATH = '/home/rpl/workspace/BIO_workcell/bio_workcell/workflows/growth_curve/create_plate_T0.yaml'
+READ_PLATE_T12_FILE_PATH = '/home/rpl/workspace/BIO_workcell/bio_workcell/workflows/growth_curve/read_plate_T12.yaml'
+
+DISPOSE_BOX_PLATE_FILE_PATH = '/home/rpl/workspace/BIO_workcell/bio_workcell/workflows/growth_curve/dispose_box_plate.yaml'
+DISPOSE_GROWTH_MEDIA_FILE_PATH = '/home/rpl/workspace/BIO_workcell/bio_workcell/workflows/growth_curve/dispose_growth_media.yaml'
+
+HIDEX_OPEN_CLOSE_FILE_PATH = '/home/rpl/workspace/BIO_workcell/bio_workcell/workflows/growth_curve/open_close_hidex.yaml'
+
 times = []
 iterations = 0
 removals = 0
 
 def main():
-    six() 
+    while(iterations < EXPERIMENT_ITERATIONS or len(times) == 0):
+        if(iterations < EXPERIMENT_ITERATIONS):
+            setup(iterations)
+            liconic_id = iterations + 1
+            T0_Reading(liconic_id)
+            times.append(round(time.time))
+            iterations = iterations + 1
+            if(iterations % 2 == 0):
+                dispose(iterations)
+            #hidex_refresh_time = round(time.time)
+        if(round(time.time) - times[0] > INCUBATION_TIME_SECONDS):
+            liconic_id = removals + 1
+            T12_Reading(liconic_id)
+            times.pop(0)
+            removals = removals + 1
+            #hidex_refresh_time = round(time.time)
+        #if(round(time.time) - hidex_refresh_time < (HIDEX_IDLE_THRESHOLD_SECONDS - 20*60)):
+            #refreshHidex()
 
 def two():
     hidex_refresh_time = round(time.time)
     EXPERIMENT_ITERATIONS = 2
     while(iterations < EXPERIMENT_ITERATIONS or len(times) == 0):
         if(iterations < EXPERIMENT_ITERATIONS):
-            if(iterations % 2 == 0):
-                setup(True)
-            else: 
-                setup(False)  
+            setup(iterations)
             liconic_id = iterations + 1
-            tZero(liconic_id)
+            T0_Reading(liconic_id)
             times.append(round(time.time))
             iterations = iterations + 1
             if(iterations % 2 == 0):
@@ -43,7 +70,7 @@ def two():
             #hidex_refresh_time = round(time.time)
         if(round(time.time) - times[0] > INCUBATION_TIME_SECONDS):
             liconic_id = removals + 1
-            tOne(liconic_id)
+            T12_Reading(liconic_id)
             times.pop(0)
             removals = removals + 1
             #hidex_refresh_time = round(time.time)
@@ -56,12 +83,9 @@ def six():
     EXPERIMENT_ITERATIONS = 2
     while(iterations < EXPERIMENT_ITERATIONS or len(times) == 0):
         if(iterations < EXPERIMENT_ITERATIONS):
-            if(iterations % 2 == 0):
-                setup(True)
-            else: 
-                setup(False)
+            setup(iterations)
             liconic_id = iterations + 1
-            tZero(liconic_id)
+            T0_Reading(liconic_id)
             times.append(round(time.time))
             iterations = iterations + 1
             if(iterations % 2 == 0):
@@ -69,56 +93,74 @@ def six():
             #hidex_refresh_time = round(time.time)
         if(round(time.time) - times[0] > INCUBATION_TIME_SECONDS):
             liconic_id = removals + 1
-            tOne(liconic_id)
+            T12_Reading(liconic_id)
             times.pop(0)
             removals = removals + 1
             #hidex_refresh_time = round(time.time)
         #if(round(time.time) - hidex_refresh_time < (HIDEX_IDLE_THRESHOLD_SECONDS - 20*60)):
             #refreshHidex()
 
-def dispose():
-    wf_path = Path('/home/rpl/workspace/BIO_workcell/bio_workcell/workflows/growth_curve/dispose_box_plate.yaml')
-    wei_client = WEI(wf_config = wf_path.resolve(), workcell_log_level=logging.ERROR, workflow_log_level=logging.ERROR)
-    run_info = wei_client.run_workflow(payload=None)
-    print(run_info)
+def twelve():
+    while(iterations < EXPERIMENT_ITERATIONS or len(times) == 0):
+        if(iterations < EXPERIMENT_ITERATIONS):
+            setup(iterations)
+            liconic_id = iterations + 1
+            T0_Reading(liconic_id)
+            times.append(round(time.time))
+            iterations = iterations + 1
+            if(iterations % 2 == 0):
+                dispose(iterations)
+            #hidex_refresh_time = round(time.time)
+        if(round(time.time) - times[0] > INCUBATION_TIME_SECONDS):
+            liconic_id = removals + 1
+            T12_Reading(liconic_id)
+            times.pop(0)
+            removals = removals + 1
+            #hidex_refresh_time = round(time.time)
+        #if(round(time.time) - hidex_refresh_time < (HIDEX_IDLE_THRESHOLD_SECONDS - 20*60)):
+            #refreshHidex()
+    
+def dispose(iterations):
+    disposal_index = "Stack2"
+    stack_type = iterations/2
+    if(stack_type <= 2):
+        disposal_index = "LidNest", stack_type
+    if(stack_type == 4):
+        disposal_index = "LidNest", 3
+    payload={
+        'disposal_location':  disposal_index,    
+        }
+    run_WEI(DISPOSE_BOX_PLATE_FILE_PATH, payload)
+    if(stack_type % 3 == 0):
+        run_WEI(DISPOSE_GROWTH_MEDIA_FILE_PATH, None)
 
-def setup(tip_box_and_growth_media):
-    if(tip_box_and_growth_media):
-        wf_path = Path('/home/rpl/workspace/BIO_workcell/bio_workcell/workflows/growth_curve/complete_hudson_setup.yaml') 
-        wei_client = WEI(wf_config = wf_path.resolve(), workcell_log_level=logging.ERROR, workflow_log_level=logging.ERROR)
-        run_info = wei_client.run_workflow(payload=None)
-        print(run_info)
-
-    else:
-        wf_path = Path('/home/rpl/workspace/BIO_workcell/bio_workcell/workflows/growth_curve/streamlined_hudson_setup.yaml') 
-        wei_client = WEI(wf_config = wf_path.resolve(), workcell_log_level=logging.ERROR, workflow_log_level=logging.ERROR)
-        run_info = wei_client.run_workflow(payload=None)
-        print(run_info)
+def setup(iterations):
+    if(iterations % 2 == 0):
+        run_WEI(COMPLETE_HUDSON_SETUP_FILE_PATH, None)
+        if(iterations % 6 == 0):
+            LidNest_index = 3 - iterations/6
+            payload={
+                'lidnest_index':  LidNest_index,    
+            }
+            run_WEI(SETUP_GROWTH_MEDIA_FILE_PATH, payload)
+    else: 
+        run_WEI(STREAMLINED_HUDSON_SETUP_FILE_PATH, None)
 
 def base():
     while(iterations < EXPERIMENT_ITERATIONS or len(times) == 0):
         if(iterations < EXPERIMENT_ITERATIONS):   
-            tZero()
+            T0_Reading()
             times.append(round(time.time))
             iterations = iterations + 1
         if(round(time.time) - times[0] > INCUBATION_TIME_SECONDS):
-            tOne()
+            T12_Reading()
             times.pop(0)
         
 def refreshHidex():
-    wf_path = Path('/home/rpl/workspace/BIO_workcell/bio_workcell/workflows/growth_curve/open_close_hidex.yaml') #Open_Close_Hidex File Path
+    run_WEI(HIDEX_OPEN_CLOSE_FILE_PATH, None)
 
-    wei_client = WEI(wf_config = wf_path.resolve(), workcell_log_level=logging.ERROR, workflow_log_level=logging.ERROR)
-
-    run_info = wei_client.run_workflow(payload=None)
-    print(run_info)
-
-def tZero(liconic_plate_id):
-    wf_path = Path('/home/rpl/workspace/BIO_workcell/bio_workcell/workflows/growth_curve/create_plate_T0.yaml') 
-
-    wei_client = WEI(wf_config = wf_path.resolve(), workcell_log_level=logging.ERROR, workflow_log_level=logging.ERROR)
+def T0_Reading(liconic_plate_id):
     plate_id = '', liconic_plate_id
-
     payload={
         'temp': 37.0, 
         'humidity': 95.0,
@@ -151,23 +193,11 @@ def tZero(liconic_plate_id):
     payload['hso_3_lines'] = hso_3_lines
     payload['hso_3_basename'] = hso_3_basename
 
-    # #run Growth Create Plate
-    run_info = wei_client.run_workflow(payload=payload)
-    print(run_info)
-    # # store plate_n, payload, and time into a db
-    # # publish flow
-    # # loop here
-    # ###################
-    # #check if any plate on db has 12h
-    # #create new payload
-    # #run measure_plate
-    # #publish again
-    # #loop here
+    #run Growth Create Plate
+    run_WEI(CREATE_PLATE_T0_FILE_PATH, payload)
 
-def tOne(liconic_plate_id):
-    wf_path = Path('/home/rpl/workspace/BIO_workcell/bio_workcell/workflows/growth_curve/read_plate_T12.yaml')
-    wei_client = WEI(wf_config = wf_path.resolve(), workcell_log_level=logging.ERROR, workflow_log_level=logging.ERROR)
 
+def T12_Reading(liconic_plate_id):
     plate_id = '', liconic_plate_id
 
     payload={
@@ -203,8 +233,8 @@ def tOne(liconic_plate_id):
     payload['hso_3_basename'] = hso_3_basename
 
     # #run Growth Create Plate
-    run_info = wei_client.run_workflow(payload=payload)
-    print(run_info)
+    run_WEI(READ_PLATE_T12_FILE_PATH, payload)
+
     # # store plate_n, payload, and time into a db
     # # publish flow
     # # loop here
@@ -214,5 +244,12 @@ def tOne(liconic_plate_id):
     # #run measure_plate
     # #publish again
     # #loop here
+
+def run_WEI(file_location, payload_class):
+    wf_path = Path(file_location)
+    wei_client = WEI(wf_config = wf_path.resolve(), workcell_log_level=logging.ERROR, workflow_log_level=logging.ERROR)
+    run_info = wei_client.run_workflow(payload=payload_class)
+    print(run_info)
+
 if __name__ == "__main__":
     main()
