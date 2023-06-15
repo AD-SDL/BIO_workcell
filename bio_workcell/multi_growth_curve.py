1fb44ea35459268093905b33fe44d53a8625a16c
Casey Stone <cstone@anl.gov>
2023-06-15 15:18:32 -0500

    Updated Wei 2 and hidex reader

--- bio_workcell/multi_growth_curve.py
+++ bio_workcell/multi_growth_curve.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python3
 
 import logging
-from pathlib import Path
 from argparse import ArgumentParser
 import time
-from rpl_wei.wei_workcell_base import WEI
-from .tools.c2_flow import c2_flow
+from tools.c2_flow import c2_flow
 from pathlib import Path
-
+#from rpl_wei.wei_workcell_base import WEI
 from workflows.growth_curve.hso_functions import package_hso
 from workflows.growth_curve import solo_step1, solo_step2, solo_step3
+from rpl_wei import Experiment
 
-EXPERIMENT_ITERATIONS = 12
-INCUBATION_TIME_HOURS = 12
+EXPERIMENT_ITERATIONS = 2
+INCUBATION_TIME_HOURS = 0.5
 INCUBATION_TIME_MINUTES = INCUBATION_TIME_HOURS *  60
 INCUBATION_TIME_SECONDS = INCUBATION_TIME_MINUTES * 60
 TARGETED_PROCESS_TIME_MINUTES = 60
@@ -32,10 +31,15 @@ DISPOSE_GROWTH_MEDIA_FILE_PATH = '/home/rpl/workspace/BIO_workcell/bio_workcell/
 HIDEX_OPEN_CLOSE_FILE_PATH = '/home/rpl/workspace/BIO_workcell/bio_workcell/workflows/growth_curve/open_close_hidex.yaml'
 
 incubation_start_times = []
-iterations = 0
-removals = 0
 
-def main():
+
+exp = Experiment('127.0.0.1', '8000', 'Growth_Curve')
+exp.register_exp() 
+exp.events.log_local_compute("package_hso")
+
+def main(): 
+    iterations = 0
+    removals = 0
     while(iterations < EXPERIMENT_ITERATIONS or len(incubation_start_times) == 0):
         if(iterations < EXPERIMENT_ITERATIONS):
             setup(iterations)
@@ -127,21 +131,25 @@ def dispose(iterations):
     payload={
         'disposal_location':  disposal_index,    
         }
-    run_WEI(DISPOSE_BOX_PLATE_FILE_PATH, payload)
+    run_WEI(DISPOSE_BOX_PLATE_FILE_PATH, payload, False)
     if(stack_type % 3 == 0):
-        run_WEI(DISPOSE_GROWTH_MEDIA_FILE_PATH, None)
+        run_WEI(DISPOSE_GROWTH_MEDIA_FILE_PATH, None, False)
 
 def setup(iterations):
     if(iterations % 2 == 0):
-        run_WEI(COMPLETE_HUDSON_SETUP_FILE_PATH, None)
+        complete_payload={
+                'tip_box_position': 3    
+            }
+        run_WEI(COMPLETE_HUDSON_SETUP_FILE_PATH, complete_payload, False)
         if(iterations % 6 == 0):
             LidNest_index = 3 - iterations/6
             payload={
-                'lidnest_index':  LidNest_index,    
+                'lidnest_index':  LidNest_index,
+                'tip_box_position': 3    
             }
-            run_WEI(SETUP_GROWTH_MEDIA_FILE_PATH, payload)
+            run_WEI(SETUP_GROWTH_MEDIA_FILE_PATH, payload, False)
     else: 
-        run_WEI(STREAMLINED_HUDSON_SETUP_FILE_PATH, None)
+        run_WEI(STREAMLINED_HUDSON_SETUP_FILE_PATH, None, False)
 
 def base():
     while(iterations < EXPERIMENT_ITERATIONS or len(incubation_start_times) == 0):
@@ -154,7 +162,7 @@ def base():
             incubation_start_times.pop(0)
         
 def refreshHidex():
-    run_WEI(HIDEX_OPEN_CLOSE_FILE_PATH, None)
+    run_WEI(HIDEX_OPEN_CLOSE_FILE_PATH, None, False)
 
 def T0_Reading(liconic_plate_id):
     plate_id = '', liconic_plate_id
@@ -191,7 +199,7 @@ def T0_Reading(liconic_plate_id):
     payload['hso_3_basename'] = hso_3_basename
 
     #run Growth Create Plate
-    run_WEI(CREATE_PLATE_T0_FILE_PATH, payload)
+    run_WEI(CREATE_PLATE_T0_FILE_PATH, payload, True)
 
 
 def T12_Reading(liconic_plate_id):
@@ -230,7 +238,7 @@ def T12_Reading(liconic_plate_id):
     payload['hso_3_basename'] = hso_3_basename
 
     # #run Growth Create Plate
-    run_WEI(READ_PLATE_T12_FILE_PATH, payload)
+    run_WEI(READ_PLATE_T12_FILE_PATH, payload, True)
 
     # # store plate_n, payload, and time into a db
     # # publish flow
@@ -242,11 +250,31 @@ def T12_Reading(liconic_plate_id):
     # #publish again
     # #loop here
 
-def run_WEI(file_location, payload_class):
-    wf_path = Path(file_location)
-    wei_client = WEI(wf_config = wf_path.resolve(), workcell_log_level=logging.ERROR, workflow_log_level=logging.ERROR)
-    run_info = wei_client.run_workflow(payload=payload_class)
-    print(run_info)
+def run_WEI(file_location, payload_class, Hidex_Used):
+    flow_info = exp.run_job(Path(file_location).resolve(), payload=payload_class, simulate=False)
+
+    flow_status = exp.query_job(flow_info["job_id"])
+    while(flow_status["status"] != "finished" and flow_status["status"] != "failure"):
+        flow_status = exp.query_job(flow_info["job_id"])
+        time.sleep(3)
+
+    run_info = flow_status["result"]
+    run_info["run_dir"] = Path(run_info["run_dir"])
+
+    if Hidex_Used:
+        print(run_info)
+        hidex_file_path = run_info["hist"]["run Hidex"]["action_msg"]
+        hidex_file_path = hidex_file_path.replace('\\', '/')
+        hidex_file_path = hidex_file_path.replace("C:/", "/C/")
+        flow_title = Path(hidex_file_path) #Path(run_info["hist"]["run_assay"]["step_response"])
+        fname = flow_title.name
+        flow_title = flow_title.parents[0]
+
+        c2_flow("hidex_test", str(fname.split('.')[0]), hidex_file_path, flow_title, fname, exp)
 
 if __name__ == "__main__":
     main()
+
+#!/usr/bin/env python3
+
+
